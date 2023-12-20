5acaeb5b2c698d8fa3b2601a84d96c42b20f834d
Abdellatif <abdelt.2012@gmail.com>
2023-12-20 03:08:38 +0100

    Task done.

--- sort.h
+++ sort.h
@@ -36,10 +36,12 @@ typedef struct listint_s
 } listint_t;
 
 /* given functions */
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 /* algoritms */
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
